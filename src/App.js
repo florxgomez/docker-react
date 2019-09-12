8c7e530d44dd6bebd97222f4da3febbbbe4151fc
Flor <florxgomez@gmail.com>
2019-09-11 22:51:13 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>I have changed!
         </p>
         <a
           className="App-link"
